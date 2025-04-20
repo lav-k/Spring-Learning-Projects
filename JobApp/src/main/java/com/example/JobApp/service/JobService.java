8eeee92581283b869bc3a6c5789671983a7db5c3
Lav kush <53362469+lav-k@users.noreply.github.com>
2025-04-20 23:38:36 +0530

    Add files via upload
    Job Portal Application where users recruiter can post the jobs and job seekers can view the jobs.
    
    We have used jsp, springboot.

---
+++ JobApp/src/main/java/com/example/JobApp/service/JobService.java
@@ -0,0 +1,22 @@
+package com.example.JobApp.service;
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.repository.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRepo repo;
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs(){
+        return repo.getAllJobs();
+    }
+}
